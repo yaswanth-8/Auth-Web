a88427f2ffcb830a5bfd0ef404ec0b28eb9ee868
yaswanth <ya8wanth@gmail.com>
2023-04-20 13:48:47 +0530

    Add project files.

---
+++ Auth-Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Auth_Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth_Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
---
+++ Auth-Web/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth_Web.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
